beec335bd35a553b80a2537a44ed1c24267051f4
Etienne <etienne@mapwize.io>
2020-04-27 09:34:07 +0200

    Update demo app using MapwizeUI

--- app/src/main/java/io/indoorlocation/socket/demoapp/MapActivity.java
+++ app/src/main/java/io/indoorlocation/socket/demoapp/MapActivity.java
@@ -2,22 +2,141 @@ package io.indoorlocation.socket.demoapp;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.FrameLayout;
 
-import com.mapbox.mapboxsdk.Mapbox;
-import com.mapbox.mapboxsdk.maps.MapView;
-import com.mapbox.mapboxsdk.maps.MapboxMap;
-import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import java.util.Locale;
 
 import io.indoorlocation.socketlocationprovider.SocketIndoorLocationProvider;
-import io.mapwize.mapwizeformapbox.MapOptions;
-import io.mapwize.mapwizeformapbox.MapwizePlugin;
+import io.mapwize.mapwizesdk.api.MapwizeObject;
+import io.mapwize.mapwizesdk.map.MapOptions;
+import io.mapwize.mapwizesdk.map.MapwizeMap;
+import io.mapwize.mapwizeui.MapwizeFragment;
 
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity implements MapwizeFragment.OnFragmentInteractionListener {
+
+    private MapwizeFragment mapwizeFragment;
+    private static final int MY_PERMISSION_ACCESS_FINE_LOCATION = 0;
+
+    MapwizeMap mapwizeMap;
+    private SocketIndoorLocationProvider socketIndoorLocationProvider;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        FrameLayout container = findViewById(R.id.container);
+
+        MapOptions opts = new MapOptions.Builder()
+                .language(Locale.getDefault().getLanguage()).build();
+        mapwizeFragment = MapwizeFragment.newInstance(opts);
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(container.getId(), mapwizeFragment);
+        ft.commit();
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapwizeFragment.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapwizeFragment.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        mapwizeFragment.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        mapwizeFragment.onStop();
+        super.onStop();
+    }
+
+    @Override
+    public void onSaveInstanceState(@androidx.annotation.NonNull Bundle saveInstanceState) {
+        super.onSaveInstanceState(saveInstanceState);
+        mapwizeFragment.onSaveInstanceState(saveInstanceState);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapwizeFragment.onLowMemory();
+    }
+
+    @Override
+    public void onDestroy() {
+        mapwizeFragment.onDestroy();
+        super.onDestroy();
+    }
+
+    private void startLocationService() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSION_ACCESS_FINE_LOCATION);
+        }
+        else {
+            setupLocationProvider();
+        }
+    }
+
+    private void setupLocationProvider() {
+        socketIndoorLocationProvider = new SocketIndoorLocationProvider(this, SocketIndoorLocationProviderDemoApp.SOCKET_SERVER_URL);
+        socketIndoorLocationProvider.start();
+        mapwizeMap.setIndoorLocationProvider(socketIndoorLocationProvider);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @androidx.annotation.NonNull String permissions[], @NonNull int[] grantResults) {
+        if (requestCode == MY_PERMISSION_ACCESS_FINE_LOCATION) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                setupLocationProvider();
+            }
+        }
+    }
+
+    @Override
+    public void onMenuButtonClick() {
+        Log.i("MapActivity", "onMenuButtonClick");
+    }
+
+    @Override
+    public void onInformationButtonClick(MapwizeObject mapwizeObject) {
+        Log.i("MapActivity", "onInformationButtonClick");
+    }
+
+    @Override
+    public void onFragmentReady(MapwizeMap mapwizeMap) {
+        this.mapwizeMap = mapwizeMap;
+        startLocationService();
+    }
+
+    @Override
+    public void onFollowUserButtonClickWithoutLocation() {
+        Log.i("MapActivity", "onFollowUserButtonClickWithoutLocation");
+    }
+
+}
+
+/*public class MapActivity extends AppCompatActivity {
 
     private MapView mapView;
     private MapwizePlugin mapwizePlugin;
@@ -104,4 +223,4 @@ public class MapActivity extends AppCompatActivity {
         super.onSaveInstanceState(outState);
         mapView.onSaveInstanceState(outState);
     }
-}
+}*/
--- app/src/main/java/io/indoorlocation/socket/demoapp/SocketIndoorLocationProviderDemoApp.java
+++ app/src/main/java/io/indoorlocation/socket/demoapp/SocketIndoorLocationProviderDemoApp.java
@@ -2,14 +2,25 @@ package io.indoorlocation.socket.demoapp;
 
 import android.app.Application;
 
-import io.mapwize.mapwizeformapbox.AccountManager;
+import com.mapbox.mapboxsdk.Mapbox;
+
+import io.mapwize.mapwizesdk.core.MapwizeConfiguration;
 
 public class SocketIndoorLocationProviderDemoApp extends Application {
+
+    // This is a demo key, giving you access to the demo building. It is not allowed to use it for production.
+    // The key might change at any time without notice. Get your key by signin up at mapwize.io
+    static final String MAPWIZE_API_KEY = "YOUR_MAPWIZE_API_KEY";
+    static final String SOCKET_SERVER_URL = "YOUR_SOCKET_SERVER_URL";
+
 
     @Override
     public void onCreate() {
         super.onCreate();
-        AccountManager.start(this, "1f04d780dc30b774c0c10f53e3c7d4ea");// PASTE YOU MAPWIZE API KEY HERE !!! This is a demo key, giving you access to the demo building. It is not allowed to use it for production. The key might change at any time without notice. Get your key by signin up at mapwize.io
+        Mapbox.getInstance(this, "pk.mapwize");
+        // Mapwize globale initialization
+        MapwizeConfiguration config = new MapwizeConfiguration.Builder(this, MAPWIZE_API_KEY).build();
+        MapwizeConfiguration.start(config);
     }
 
 }
